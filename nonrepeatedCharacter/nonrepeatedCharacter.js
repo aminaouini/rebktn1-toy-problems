8fcb4473ed8f5ecea8eacec6e188e699d984c6e2
aminaouini <aminaouini565@gmail.com>
2019-11-25 09:45:45 +0100

    a

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,24 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+  let letter = string[0];
+  let letters = {};
+  let count = 0;
+  let take = 0;
+  for (let i = 0; i < string.length; i++) {
+  	if (i < string.length -1){
+  		if(letter === string[i]) {
+  	  	letter = letters[take];
+  	  	take++;
+  	  	// i = letterIndex || i+1
+  	  	
+  	  	}
+  	  	if (letter !== string[i]) {
+  	  		letterIndex[count] = string[i];
+  	  		count++;
+  	  	}
+  	}
+
+  }
+  return letter;
 };
