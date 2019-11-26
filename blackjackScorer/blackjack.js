3ed543946fbfc4e1c096860d224fd7db20ae86fc
aminaouini <aminaouini565@gmail.com>
2019-11-26 09:05:47 +0100

    done

--- blackjackScorer/blackjack.js
+++ blackjackScorer/blackjack.js
@@ -13,3 +13,33 @@ Examples
 ["A", "10", "A"]                ==>  12
 ["5", "3", "7"]                 ==>  15
 ["5", "4", "3", "2", "A", "K"]  ==>  25 */
+
+function blackJack(array) {
+	var result = 0;
+	for (let i = 0; i < array.length; i++ ) {
+		if(array[i] === 'J' || array[i] === 'Q' || array[i] === 'K') result += 10
+		if(array[i] === '10') result += 10 
+		if(array[i] === '9') result += 7 
+		if(array[i] === '8') result += 8 
+		if(array[i] === '7') result += 7 
+		if(array[i] === '6') result += 6 
+		if(array[i] === '5') result += 5
+		if(array[i] === '4') result += 4
+		if(array[i] === '3') result += 3
+		if(array[i] === '2') result += 2
+		if(array[i] === 'A') {
+			var x;
+			for (let i = 0; i < array.length; i++ ) {
+				if (array[i] === 10) x = 10;
+				if (array[i] === 'K') x = 'K';
+			}
+			if (x === 10 ||x === 'K') {
+				result += 1;
+			}else {
+				result += 11;
+			}
+		}
+	}
+	
+	return result;
+}
